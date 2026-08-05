087f240e5218392bc4849c584ebf8061a4b3fa3e
lecielgit <93504208+lecielgit@users.noreply.github.com>
2026-08-05 08:05:22 +0800

    Update build25.sh

--- rockchip/build25.sh
+++ rockchip/build25.sh
@@ -55,10 +55,6 @@ PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
-PACKAGES="$PACKAGES ip6tables"
-PACKAGES="$PACKAGES kmod-ipt-nat6"
-PACKAGES="$PACKAGES ip6tables-mod-nat"
-PACKAGES="$PACKAGES odhcp6c"
 
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
